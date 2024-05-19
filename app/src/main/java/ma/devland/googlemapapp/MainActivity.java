a5155ff7e3ba03e436625b83e3029cac0c482172
ElMehdi WADDAD <wadadmehdi11@gmail.com>
2024-05-19 19:49:51 +0100

    commit pass

---
+++ app/src/main/java/ma/devland/googlemapapp/MainActivity.java
@@ -0,0 +1,45 @@
+package ma.devland.googlemapapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap myMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+
+        LatLng sydney = new LatLng(-34,151);
+        myMap.addMarker(new MarkerOptions().position(sydney).title("sydeney"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+    }
+}
